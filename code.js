082d98577b57c2ab68dd16a416d5618257d33039
TC <nothing@nothing.com>
2025-01-08 11:20:07 +1300

    Refactored to optimise code further

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const knightMoveset = [
+const moves = [
   [2, 1],
   [2, -1],
   [-2, 1],
@@ -10,35 +10,41 @@ const knightMoveset = [
 ];
 
 function knightMoves(start, end) {
-  const queue = [[start[0], start[1], 0, [[start[0], start[1]]]]];
+  const queue = [[start[0], start[1], 0]];
   const visited = new Set();
+  const previous = {};
   visited.add(start.toString());
 
   while (queue.length > 0) {
-    counter++;
-    const [x, y, steps, currentSteps] = queue.shift();
+    const [x, y, steps] = queue.shift();
     if (x == end[0] && y == end[1]) {
-      console.log(`It took ${steps}steps. Here was your path:`);
-      currentSteps.forEach((step, index) => {
-        console.log(`${index}: [${step[0]},${step[1]}]`);
+      console.log(`${steps} steps were taken, below is your path:`);
+      let current = `${x},${y}`;
+      const path = [current];
+      while (previous[current]) {
+        path.push(previous[current]);
+        current = previous[current];
+      }
+      // As we found found path by going in reverse, we have to reverse order
+      path.reverse();
+
+      path.forEach((move, index) => {
+        console.log(`${index}: ${move}`);
       });
-      break;
+
+      return;
     }
 
-    knightMoveset.forEach((move) => {
+    moves.forEach((move) => {
       const newX = x + move[0];
       const newY = y + move[1];
-      if (
-        newX < 0 ||
-        newX > 8 ||
-        newY < 0 ||
-        newY > 8 ||
-        visited.has([newX, newY].toString())
-      ) {
-        // Do nothing
+      const key = `${newX},${newY}`;
+      if (newX < 0 || newY < 0 || visited.has(key) || newX > 8 || newY > 8) {
+        // Skip
       } else {
-        visited.add([newX, newY].toString());
-        queue.push([newX, newY, steps + 1, [...currentSteps, [newX, newY]]]);
+        visited.add(key);
+        previous[key] = `${x},${y}`;
+        queue.push([newX, newY, steps + 1]);
       }
     });
   }
