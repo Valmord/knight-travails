0ff4b61e9291687376f4d3cc0571d791dc5ec719
TC <nothing@nothing.com>
2025-01-07 18:35:39 +1300

    Refactor and remove unneeded code

--- code.js
+++ code.js
@@ -1,62 +1,46 @@
-const board = [
-  [0, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 1, 1, 1, 1, 1, 1],
-  [1, 1, 0, 1, 1, 1, 1, 1],
-  [1, 1, 1, 0, 1, 1, 1, 1],
-  [1, 1, 1, 1, 0, 1, 1, 1],
-  [1, 1, 1, 1, 1, 0, 1, 1],
-  [1, 1, 1, 1, 1, 1, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 0],
+const knightMoveset = [
+  [2, 1],
+  [2, -1],
+  [-2, 1],
+  [-2, -1],
+  [1, 2],
+  [1, -2],
+  [-1, 2],
+  [-1, -2],
 ];
 
-function validMoves(x, y, steps, currentSteps) {
-  const moves = [
-    [2, 1],
-    [2, -1],
-    [-2, 1],
-    [-2, -1],
-    [1, 2],
-    [1, -2],
-    [-1, 2],
-    [-1, -2],
-  ];
+function knightMoves(start, end) {
+  const queue = [[start[0], start[1], 0, [[start[0], start[1]]]]];
+  const visited = new Set();
+  visited.add(start.toString());
 
-  const validQueue = [];
-
-  moves.forEach((move) => {
-    const xMove = x + move[0];
-    const yMove = y + move[1];
-    if (xMove < 0 || xMove > 8 || yMove < 0 || yMove > 8) {
-      // Do nothing
-      // console.log(xMove, yMove);
-    } else
-      validQueue.push([
-        xMove,
-        yMove,
-        steps + 1,
-        [...currentSteps, [xMove, yMove]],
-      ]);
-  });
-
-  return validQueue;
-}
-
-function knightMoves(from, to) {
-  const [startX, startY] = from;
-  const [endX, endY] = to;
-
-  const queue = [[startX, startY, 0, [[startX, startY]]]];
-
-  while (true) {
+  while (queue.length > 0) {
+    counter++;
     const [x, y, steps, currentSteps] = queue.shift();
-    if (x == endX && y == endY) {
+    if (x == end[0] && y == end[1]) {
       console.log(`It took ${steps}steps. Here was your path:`);
       currentSteps.forEach((step, index) => {
         console.log(`${index}: [${step[0]},${step[1]}]`);
       });
       break;
     }
-    queue.push(...validMoves(x, y, steps, currentSteps));
+
+    knightMoveset.forEach((move) => {
+      const newX = x + move[0];
+      const newY = y + move[1];
+      if (
+        newX < 0 ||
+        newX > 8 ||
+        newY < 0 ||
+        newY > 8 ||
+        visited.has([newX, newY].toString())
+      ) {
+        // Do nothing
+      } else {
+        visited.add([newX, newY].toString());
+        queue.push([newX, newY, steps + 1, [...currentSteps, [newX, newY]]]);
+      }
+    });
   }
 }
 
